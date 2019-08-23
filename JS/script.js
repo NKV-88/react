294f5450dca6bc199aa43b2db357f3dc5c1dee01
NKV-88 <sysadm86@gmail.com>
2019-08-23 15:00:19 +0500

    Начал 3-е

--- JS/script.js
+++ JS/script.js
@@ -45,7 +45,7 @@ const taskTag = `<h3>${t1}</h3>
 				};
 
 				console.log (calculateArea(4,'квадрат', 2,2))
-				
+
 				P/s. Результат в консоли))</pre>
 
 				<h3>${t3}</h3>
@@ -93,8 +93,37 @@ const calculateArea = (a,b,c,d) => {
 console.log (calculateArea(4,'квадрат', 2,2))
 
 
+/*3. Необходимо написать иерархию классов вида:`,
+`Human -> Employee -> Developer
+Human -> Employee -> Manager
+Каждый Менеджер (Manager) должен иметь внутренний массив своих сотрудников (разработчиков), а также методы по удалению/добавлению разработчиков.
+Каждый Разработчик (Developer) должны иметь ссылку на Менеджера и методы для изменения менеджера (имеется ввиду возможность назначить другого менеджера).
+У класса Human должны быть следующие параметры: name (строка), age (число), dateOfBirth (строка или дата)
+У класса Employee должны присутствовать параметры: salary (число), department (строка)
+В классе Human должен присутствовать метод displayInfo, который возвращает строку со всеми параметрами экземпляра Human.
+В классе Employee должен быть реализовать такой же метод (displayInfo), который вызывает базовый метод и дополняет его параметрами из экземпляра Employee
+Чтобы вызвать метод базового класса, необходимо внутри вызова метода displayInfo класса Employee написать: super.displayInfo(), это вызовет метод disaplyInfo класс Human и вернет строку с параметрами Human'a.*/
 
+class Human {
+	constructor(){
 
+	}
+}
 
+class Employee extends Human {
+	constructor(){
 
-	
+	}
+}
+
+class Developer extends Employee{
+	constructor() {
+
+	}
+}
+
+class Manager extends Employee{
+	constructor() {
+
+	} 
+}
